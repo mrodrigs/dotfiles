e38881b12bc79d2971517a456e46e61ecdf39c6d
Mauricio Rodrigues <mrodrigues.professional@gmail.com>
2026-09-06 17:42:17 -0300

    pc boot loader

--- nixos/hosts/pc/configuration.nix
+++ nixos/hosts/pc/configuration.nix
@@ -4,12 +4,15 @@
   imports = [ ../../common.nix ./hardware-configuration.nix ];
 
   networking.hostName = "pc";
-
-  boot.loader.grub.enable = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.efiInstallAsRemovable = false;
-  boot.loader.grub.useOSProber = false;
-  boot.loader.efi.canTouchEfiVariables = false;
-  boot.loader.efi.efiSysMountPoint = "/boot";
+  
+  boot.loader.systemd-boot = {
+    enable = true;
+    xbootldrMountPoint = "/boot";
+    configurationLimit = 15;
+  };
+  boot.loader.efi = {
+    efiSysMountPoint = "/efi";
+    canTouchEfiVariables = true;
+  };
+  boot.loader.timeout = 5;
 }

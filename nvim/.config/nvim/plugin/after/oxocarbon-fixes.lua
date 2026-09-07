91121c2c4b6f8dabefcd1dde60a276928d657c8a
Mauricio Rodrigues <mrodrigues.professional@gmail.com>
2026-09-07 14:49:24 -0300

    fix oxocarbon theming

--- nvim/.config/nvim/plugin/after/oxocarbon-fixes.lua
+++ nvim/.config/nvim/plugin/after/oxocarbon-fixes.lua
@@ -7,6 +7,16 @@ local function apply()
 	vim.api.nvim_set_hl(0, "@module", { fg = c.base07 })
 	vim.api.nvim_set_hl(0, "@module.builtin", { fg = c.base07 })
 	vim.api.nvim_set_hl(0, "@string.regexp", { fg = c.base07 })
+
+	vim.api.nvim_set_hl(0, "NeoTreeDirectoryIcon", { fg = c.base12 })
+	vim.api.nvim_set_hl(0, "NeoTreeDirectoryName", { fg = c.base09 })
+	vim.api.nvim_set_hl(0, "NeoTreeGitAdded", { fg = c.base07 })
+	vim.api.nvim_set_hl(0, "NeoTreeGitModified", { fg = c.base09 })
+	vim.api.nvim_set_hl(0, "NeoTreeGitDeleted", { fg = c.base10 })
+	vim.api.nvim_set_hl(0, "NeoTreeGitUntracked", { fg = c.base14 })
+	vim.api.nvim_set_hl(0, "NeoTreeGitConflict", { fg = c.base10 })
+	vim.api.nvim_set_hl(0, "NeoTreeDimText", { fg = c.base02 })
+	vim.api.nvim_set_hl(0, "NeoTreeTabInactive", { fg = c.base03, bg = c.base01 })
 end
 
 vim.api.nvim_create_autocmd("ColorScheme", {
